00dc5f8e3ff9b5f587bfeeaabd6f1fd7f29819d3
s123unny <s123unny@gmail.com>
2018-12-07 20:44:20 +0800

    fix sign extend

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -8,13 +8,11 @@ output reg [31:0] data_o;
 
 always @(instruction) begin
 	if (instruction == 7'b0100011) begin //s type
-		data_o <= {{20{data_i[31]}},data_i[30:25],data_i[11:7]};	
+		data_o <= {{21{instruction[31]}},instruction[30:25],instruction[11:7]};	
 	end
 	else begin
-		data_o <= {{20{data_i[31]}}, data_i[30:20]};
+		data_o <= {{21{instruction[31]}}, instruction[30:20]};
 	end
 end
-
-assign 
 
 endmodule
